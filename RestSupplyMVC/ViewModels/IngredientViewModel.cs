26580f7bd7d1e0c8f6f904e615634a734e3be472
EUROPE\betty.isakov <betty.isakov@toluna.com>
2019-01-10 18:36:59 +0200

    Added IngredientViewModel

---
+++ RestSupplyMVC/ViewModels/IngredientViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using RestSupplyDB.Models.Ingredient;
+
+namespace RestSupplyMVC.ViewModels
+{
+    public class IngredientViewModel
+    {
+        [Display(Name = "שם חומר גלם")] public string Name { get; set; }
+        [Display(Name = "יחידת מידה")] public string Unit { get; set; }
+    }
+}

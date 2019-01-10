885dac83ee8c5edb5be29678aac0a928c5bfa5ee
EUROPE\betty.isakov <betty.isakov@toluna.com>
2019-01-10 17:12:43 +0200

    minor changes - aded hebrew traslations to SupplierViewModel

--- RestSupplyMVC/ViewModels/SupplierViewModel.cs
+++ RestSupplyMVC/ViewModels/SupplierViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using RestSupplyDB.Models.Ingredient;
@@ -8,10 +9,15 @@ namespace RestSupplyMVC.ViewModels
 {
     public class SupplierViewModel
     {
+        [Display(Name = "שם ספק")]
         public string Name { get; set; }
+        [Display(Name = "כתובת")]
         public string Address { get; set; }
+        [Display(Name = "טלפון")]
         public string Phone { get; set; }
+        [Display(Name = "חומר גלם")]
         public string Ingredient { get; set; }
+        [Display(Name = "רשימת חומרי גלם")]
         public IEnumerable<Ingredients> IngredientList { get; set; }
     }
 }

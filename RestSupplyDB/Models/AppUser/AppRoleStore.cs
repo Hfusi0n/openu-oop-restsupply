2f7112b85f1ebaf0f1d0c3baad3bac1b2418828d
Or Groman <hazihazil@gmail.com>
2019-01-18 13:51:53 +0200

    Added custom user store and user role classes

---
+++ RestSupplyDB/Models/AppUser/AppRoleStore.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace RestSupplyDB.Models.AppUser
+{
+    public class AppRoleStore : RoleStore<AppRole, string, AppUserRole>
+    {
+        public AppRoleStore(RestSupplyDbContext context)
+            : base(context)
+        {
+        }
+    }
+}
---
+++ RestSupplyDB/Models/AppUser/AppUserStore.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace RestSupplyDB.Models.AppUser
+{
+    class AppUserStore : UserStore<AppUser, AppRole, string,
+        AppUserLogin, AppUserRole, AppUserClaim>
+    {
+        public AppUserStore(RestSupplyDbContext context)
+            : base(context)
+        {
+        }
+    }
+}

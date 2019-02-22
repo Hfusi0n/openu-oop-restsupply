f55eeb9cb6a740adf23555036a8ca9cfb6e7a9fd
EUROPE\betty.isakov <betty.isakov@toluna.com>
2019-02-22 18:58:07 +0200

    AppUser DTO + Convertor

---
+++ RestSupplyMVC/DTOs/AppUserDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using RestSupplyDB.Models.Kitchen;
+using RestSupplyDB.Models.AppUser;
+
+
+namespace RestSupplyMVC.DTOs
+{
+    public class AppUserDTO
+    {
+        public string UserId { get; set; }
+        public AppUserRole Role { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public IEnumerable<KitchenUsers> Kitchens { get; set; }
+
+    }
+
+}

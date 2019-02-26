f294d0a032356109764dfcb92a5ddb9ec32280d6
Or Groman <hazihazil@gmail.com>
2019-02-26 23:03:01 +0200

    Added supplierIngredients as a property in the dbcontext, remove is done from this property

---
+++ RestSupplyDB/Migrations/201902262053391_add_suppliering_set_migration.cs
@@ -0,0 +1,16 @@
+namespace RestSupplyDB.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class add_suppliering_set_migration : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}

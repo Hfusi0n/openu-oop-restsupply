658251360f6f8d0595b77790ec32cf9c832a8c28
EUROPE\betty.isakov <betty.isakov@toluna.com>
2019-03-09 01:46:47 +0200

    Home Controller nav - WIP

--- RestSupplyMVC/Controllers/HomeController.cs
+++ RestSupplyMVC/Controllers/HomeController.cs
@@ -3,13 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using RestSupplyDB;
+using RestSupplyMVC.Persistence;
 
 namespace RestSupplyMVC.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public HomeController()
+        {
+            _unitOfWork = new UnitOfWork(new RestSupplyDbContext());
+        }
         public ActionResult Index()
         {
+            var currentUserId = User.Identity.GetUserId();
+            var kitchens = _unitOfWork.Kitchens.GetKitchensByUserId(currentUserId);
+
             return View();
         }
 

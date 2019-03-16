30053f354f9da559bec721618b0894680efcc24b
EUROPE\betty.isakov <betty.isakov@toluna.com>
2019-03-16 18:39:57 +0200

    Identity Config minor refactor

---
+++ RestSupplyMVC/App_Start/ApplicationSignInManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using RestSupplyDB.Models.AppUser;
+
+namespace RestSupplyMVC
+{
+    // Configure the application sign-in manager which is used in this application.
+    public class ApplicationSignInManager : SignInManager<AppUser, string>
+    {
+        public ApplicationSignInManager(AppUserManager userManager, IAuthenticationManager authenticationManager)
+            : base(userManager, authenticationManager)
+        {
+        }
+
+        public override Task<ClaimsIdentity> CreateUserIdentityAsync(AppUser user)
+        {
+            return user.GenerateUserIdentityAsync((AppUserManager)UserManager);
+        }
+
+        public static ApplicationSignInManager Create(IdentityFactoryOptions<ApplicationSignInManager> options, IOwinContext context)
+        {
+            return new ApplicationSignInManager(context.GetUserManager<AppUserManager>(), context.Authentication);
+        }
+    }
+}
--- RestSupplyMVC/App_Start/IdentityConfig.cs
+++ RestSupplyMVC/App_Start/IdentityConfig.cs
@@ -5,10 +5,6 @@ using Microsoft.AspNet.Identity.Owin;
 using RestSupplyDB;
 using RestSupplyDB.Models;
 using RestSupplyDB.Models.AppUser;
-using Microsoft.Owin.Security;
-using System.Security.Claims;
-using System.Threading.Tasks;
-using System;
 using Microsoft.Owin;
 using Owin;
 
@@ -33,77 +29,4 @@ namespace RestSupplyMVC
     }
 
 
-    // Configure the application sign-in manager which is used in this application.
-    public class ApplicationSignInManager : SignInManager<AppUser, string>
-    {
-        public ApplicationSignInManager(AppUserManager userManager, IAuthenticationManager authenticationManager)
-            : base(userManager, authenticationManager)
-        {
-        }
-
-        public override Task<ClaimsIdentity> CreateUserIdentityAsync(AppUser user)
-        {
-            return user.GenerateUserIdentityAsync((AppUserManager)UserManager);
-        }
-
-        public static ApplicationSignInManager Create(IdentityFactoryOptions<ApplicationSignInManager> options, IOwinContext context)
-        {
-            return new ApplicationSignInManager(context.GetUserManager<AppUserManager>(), context.Authentication);
-        }
-    }
-
-    public class AppUserManager : UserManager<AppUser,string>
-    {
-        public AppUserManager(IUserStore<AppUser,string> store)
-            : base(store)
-        {
-        }
-
-        public static AppUserManager Create(IdentityFactoryOptions<AppUserManager> options, IOwinContext context)
-        {
-            var manager = new AppUserManager(new UserStore<AppUser,AppRole,string,AppUserLogin,AppUserRole,AppUserClaim>(context.Get<RestSupplyDbContext>()));
-            // Configure validation logic for usernames
-            manager.UserValidator = new UserValidator<AppUser>(manager)
-            {
-                AllowOnlyAlphanumericUserNames = false,
-                RequireUniqueEmail = true
-            };
-
-            // Configure validation logic for passwords
-            manager.PasswordValidator = new PasswordValidator
-            {
-                RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
-                RequireDigit = true,
-                RequireLowercase = true,
-                RequireUppercase = true,
-            };
-
-            // Configure user lockout defaults
-            manager.UserLockoutEnabledByDefault = true;
-            manager.DefaultAccountLockoutTimeSpan = TimeSpan.FromMinutes(5);
-            manager.MaxFailedAccessAttemptsBeforeLockout = 5;
-
-            // Register two factor authentication providers. This application uses Phone and Emails as a step of receiving a code for verifying the user
-            // You can write your own provider and plug it in here.
-            manager.RegisterTwoFactorProvider("Phone Code", new PhoneNumberTokenProvider<AppUser>
-            {
-                MessageFormat = "Your security code is {0}"
-            });
-            manager.RegisterTwoFactorProvider("Email Code", new EmailTokenProvider<AppUser>
-            {
-                Subject = "Security Code",
-                BodyFormat = "Your security code is {0}"
-            });
-            //manager.EmailService = new EmailService();
-            //manager.SmsService = new SmsService();
-            var dataProtectionProvider = options.DataProtectionProvider;
-            if (dataProtectionProvider != null)
-            {
-                manager.UserTokenProvider =
-                    new DataProtectorTokenProvider<AppUser>(dataProtectionProvider.Create("ASP.NET Identity"));
-            }
-            return manager;
-        }
-    }
 }
